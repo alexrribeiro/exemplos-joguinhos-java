438b918dcddb2a14417481910c8bf325146e31d3
Alex R Ribeiro <alexramosribeiro@gmail.com>
2023-09-03 22:41:09 -0300

    Aprimoramentos Menu

--- src/Jogos.java
+++ src/Jogos.java
@@ -6,22 +6,16 @@ public class Jogos {
         PedraPapelTesoura pp = new PedraPapelTesoura();
 
         Scanner sc = new Scanner(System.in);
-        int opcao = 0;
+        int opcao;
 
         while (true) {
             System.out.println("Selecione um jogo: \n[1] Par ou ímpar \n[2] Pedra, Papel, Tesoura \n[0] Sair");
             opcao = sc.nextInt();
             switch (opcao) {
-                case 1:
-                    pi.jogar();
-                    break;
-                case 2:
-                    pp.jogar();
-                    break;
-                case 0:
-                    System.exit(0);
-                default:
-                    System.out.println("Nenhuma opção válida foi digitada!");
+                case 1 -> pi.jogar();
+                case 2 -> pp.jogar();
+                case 0 -> System.exit(0);
+                default -> System.out.println("Nenhuma opção válida foi digitada!");
             }
         }
     }
